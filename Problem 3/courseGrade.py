7a708b884d6b2e95fb42ed81297d28b193a67456
ota1953 <157063491+ota1953@users.noreply.github.com>
2024-03-14 16:01:58 -0500

    #4 is down

--- Problem 3/courseGrade.py
+++ Problem 3/courseGrade.py
@@ -1,12 +1,20 @@
 def courseGrade():
-    
     # TODO: Declare any necessary variables here. 
-      
-      
+    ave = []
+    letter_grade = "" 
+    
     # TODO: Read a file name from the user and read the tsv file here. 
-   
+    input_file = input()
+    open_file = open(input_file)
+    read_file = open_file.readlines()
    
     # TODO: Compute student grades and exam averages, then output results to a text file here. 
+    for val in read_file:
+        if isinstance(val, int):
+            ave.append(val)
+            print(ave)
+
+
     return
 
 if __name__ == "__main__":
--- Problem 4/fileNameChange.py
+++ Problem 4/fileNameChange.py
@@ -1,5 +1,14 @@
 def fileNameChange():
     #type your code here
+    input_file = input()
+    open_file = open(input_file)
+    read_file = open_file.readlines()
+
+    for line in read_file:
+        line = line.replace('photo.jpg','info.txt')
+        print(line)
+    open_file.close()
+
 
     return
 
