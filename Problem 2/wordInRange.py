304c403ede9a1c8bcb40f0699eecb2ec35673053
ota1953 <157063491+ota1953@users.noreply.github.com>
2024-03-14 14:37:23 -0500

    We did it!

--- Problem 2/wordInRange.py
+++ Problem 2/wordInRange.py
@@ -1,5 +1,22 @@
+"""
+Name: Owen Anderson
+Lab Time: THUR 2:00 - 3:15
+"""
 def wordInRange():
-    #Type your code here
-    return
+    file_input = input()
+    word1 = input()
+    word2 = input()
+    file_object = open(str(file_input))
+    read_file = file_object.readlines()
+
+
+    for line in read_file:
+        line = line.strip('\n')
+        if word1 <= line and word2 >= line:
+            print(line +" - in range")
+        else:
+            print(line + " - not in range")
+
+    file_object.close()
 if __name__ == '__main__':
     wordInRange()
